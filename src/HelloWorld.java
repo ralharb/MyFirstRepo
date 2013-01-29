a1f45e63c5d3f56cdec3c46a182054a5eacf11ce
Raghad Alharbi <ralharb@uwo.ca>
2013-01-29 02:14:22 +0000

    My first commit ever in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * HelloWorld
+ * @author Raghad Alharbi
+ *
+ */
+public class HelloWorld
+{
+   public static void main(String[] args) 
+   {
+        System.out.println("Hello World");
+   }
+}
